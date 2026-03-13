9122e4595c6139a51b1e2f313d06c8c6739788ed
nussfuellung <hasenstab.melvin@gmail.com>
2026-03-13 02:24:46 +0100

    Update run.sh

--- gonopbx/run.sh
+++ gonopbx/run.sh
@@ -2,4 +2,4 @@
 WEB_PORT=${WEB_PORT:-8080}
 echo "Starting GonoPBX on port $WEB_PORT"
 export GONOPBX_HTTP_PORT=$WEB_PORT
-exec /usr/bin/supervisord
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
