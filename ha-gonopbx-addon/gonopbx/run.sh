4ef65042a0f478df7f6bb15e86e8d9f0672301b7
nussfuellung <hasenstab.melvin@gmail.com>
2026-03-13 01:06:43 +0100

    Enhance run.sh to log startup information
    Log the web interface port and use it for GonoPBX.

--- ha-gonopbx-addon/gonopbx/run.sh
+++ ha-gonopbx-addon/gonopbx/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Starting GonoPBX..."
+WEB_PORT=$(bashio::config 'web_port')
+
+bashio::log.info "Starting GonoPBX..."
+bashio::log.info "Webinterface Port: $WEB_PORT"
+
+export GONOPBX_HTTP_PORT=$WEB_PORT
 
 exec /usr/bin/supervisord
